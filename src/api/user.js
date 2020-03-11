a30d169466fc1d138f396a50d74c2de246ad0ba8
sekarlm <larasatisekar9@gmail.com>
2020-03-11 21:55:43 +0700

    modify userrole

---
+++ src/api/user.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+import { API } from '../config';
+
+export const getAllUsers = () => {
+    return new Promise((resolve, reject) => {
+        axios({
+            method: 'get',
+            url: API + '/userrole'
+        }).then((res) => {
+            resolve(res);
+        }).catch((err) => {
+            reject(err);
+        });
+    });
+};
+
+export const postUser = (id, role) => {
+    console.log('postUser')
+    return new Promise((resolve, reject) => {
+        axios({
+            method: 'post',
+            url: API + '/userrole',
+            data: {
+                "id": id,
+                "role": role,
+            },
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then((res) => {
+            resolve(res);
+        }).catch((err) => {
+            reject(err);
+        });
+    });
+};
